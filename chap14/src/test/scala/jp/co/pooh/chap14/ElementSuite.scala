2c69e50ffde63113d44b9487f4fbf6127b23d49f
yutax77 <yutax77@gmail.com>
2011-08-15 21:41:13 +0900

    List14.4

--- chap14/src/test/scala/jp/co/pooh/chap14/ElementSuite.scala
+++ chap14/src/test/scala/jp/co/pooh/chap14/ElementSuite.scala
@@ -1,8 +1,9 @@
 package jp.co.pooh.chap14
 import org.scalatest.Suite
 import Element.elem
-class ElementSuite extends Suite{
-	def testUniformElement() {
+import org.scalatest.FunSuite
+class ElementSuite extends FunSuite{
+	test("elem result should have passed width") {
 	  val ele = elem('x', 2, 3)
 	  assert(ele.width == 2)
 	}
