34e96464dd460fc0ccf42b870c8bb94456a2757d
yutax77 <yutax77@gmail.com>
2011-08-15 20:55:48 +0900

    List14.1

--- chap14/src/main/scala/jp/co/pooh/chap14/Element.scala
+++ chap14/src/main/scala/jp/co/pooh/chap14/Element.scala
@@ -7,6 +7,7 @@ abstract class Element {
 	def above(that: Element): Element = {
 		val this1 = this widen that.width
 		val that1 = that widen this.width
+		assert(this1.width == that1.width)
 		elem(this1.contents ++ that1.contents)
 	}
 	def beside(that: Element): Element = {
