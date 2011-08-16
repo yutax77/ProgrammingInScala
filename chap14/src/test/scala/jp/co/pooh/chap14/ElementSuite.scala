fba42c523a01d7fbe6043ca2c11fee4c88d65ee8
yutax77 <yutax77@gmail.com>
2011-08-16 13:22:04 +0900

    List14.10

--- chap14/src/test/scala/jp/co/pooh/chap14/ElementSuite.scala
+++ chap14/src/test/scala/jp/co/pooh/chap14/ElementSuite.scala
@@ -2,9 +2,10 @@ package jp.co.pooh.chap14
 import org.scalatest.testng.TestNGSuite
 import org.testng.Assert._
 import org.testng.annotations._
-
 import Element.elem
-class ElementSuite extends TestNGSuite{
+import org.scalatest.prop.Checkers
+import org.scalacheck.Prop._
+class ElementSuite extends TestNGSuite with Checkers{
     @Test
 	def verifyUniformElement() {
 	  val ele = elem('x', 2, 3)
@@ -13,5 +14,11 @@ class ElementSuite extends TestNGSuite{
 	  intercept[IllegalArgumentException] {
 	    elem('x', -2, 3)
 	  }
+    }
+    
+    @Test
+    def testUniformElement() {
+      check((w: Int) => w > 0 ==> (elem('x', w, 3).width == w))
+      check((h: Int) => h > 0 ==> (elem('x', 2, h).height == h))
     }
 }
