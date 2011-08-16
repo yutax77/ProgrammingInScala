417e4e7145aecf80b6c2a4b34eafb8e93aa686ab
yutax77 <yutax77@gmail.com>
2011-08-16 10:46:13 +0900

    List14.5

--- chap14/src/test/scala/jp/co/pooh/chap14/ElementSuite.scala
+++ chap14/src/test/scala/jp/co/pooh/chap14/ElementSuite.scala
@@ -6,15 +6,12 @@ import org.testng.annotations._
 import Element.elem
 class ElementSuite extends TestNGSuite{
     @Test
-	def testUniformElement() {
+	def verifyUniformElement() {
 	  val ele = elem('x', 2, 3)
 	  assertEquals(ele.width, 2)
-	  assertEquals(ele.height, 3)
+	  expect(3) {ele.height}
+	  intercept[IllegalArgumentException] {
+	    elem('x', -2, 3)
+	  }
     }
-    
-    @Test {
-      val expectedExceptions = 
-        Array(classOf[IllegalArgumentException])
-    }
-    def elemShouldThrowIAE() { elem('x', -2, 3)}
 }
