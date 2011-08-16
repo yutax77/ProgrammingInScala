a90b59b6df007dbdf5406645b70911a44025a1d2
yutax77 <yutax77@gmail.com>
2011-08-16 10:32:46 +0900

    List14.5 ビルドエラーがとれない

--- chap14/src/test/scala/jp/co/pooh/chap14/ElementSuite.scala
+++ chap14/src/test/scala/jp/co/pooh/chap14/ElementSuite.scala
@@ -1,17 +1,20 @@
 package jp.co.pooh.chap14
-import org.scalatest.Suite
+import org.scalatest.testng.TestNGSuite
+import org.testng.Assert._
+import org.testng.annotations._
+
 import Element.elem
-import org.scalatest.FunSuite
-import org.scalatest.junit.JUnitSuite
-import org.junit.Test
-class ElementSuite extends JUnitSuite{
+class ElementSuite extends TestNGSuite{
     @Test
 	def testUniformElement() {
 	  val ele = elem('x', 2, 3)
-	  assert(ele.width === 2)
-	  expect(3) {ele.height}
-	  intercept[IllegalArgumentException] {
-	    elem('x', -2, 3)
-	  }
-	}
+	  assertEquals(ele.width, 2)
+	  assertEquals(ele.height, 3)
+    }
+    
+    @Test {
+      val expectedExceptions = 
+        Array(classOf[IllegalArgumentException])
+    }
+    def elemShouldThrowIAE() { elem('x', -2, 3)}
 }
